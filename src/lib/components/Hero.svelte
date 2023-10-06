94cf86e5e30c6ff75d5e74ff47b04f073780593f
Yifei Han <yifeicd@gmail.com>
2023-10-06 22:23:59 +0900

    new background

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
+	import Bkg from '$lib/assets/background.png';
 </script>
 
-<div
-	class="hero h-[100vh] lg:h-[45rem] shadow-lg"
-	style="background-image: url('https://images.unsplash.com/photo-1644325349124-d1756b79dd42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2675&q=80');"
->
+<div class="hero h-[100vh] lg:h-[45rem] shadow-lg" style="background-image: url(/background.png)">
 	<div class="hero-overlay bg-opacity-70" />
 	<div class="hero-content text-neutral-content">
 		<div class="z-60 drop-shadow-lg text-center">
